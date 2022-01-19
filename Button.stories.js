f2ef17bceccc77a699c30cc4983dc0a385357856
Henry Clark <hclark9526@gmail.com>
2022-01-19 09:25:31 -0800

    base changes for storybook

---
+++ Button.stories.js
@@ -0,0 +1,57 @@
+// import Button from "../components/Button";
+import { Button } from '@hmoussa1995/react-button-lib-github';
+// import { AwesomeButton } from "react-awesome-button";
+export default {
+    title: 'Button',
+    component: Button,
+    argTypes: {
+        handleClick: { action: 'handleClick' },
+        size: { control: { type: "select", options: ["large", "default", "small", "xsmall"] }},
+        theme: {control: { type: "select", options: ["light", "dark"]}}
+    }
+}
+
+const Template = args => <Button {...args} />;
+
+export const SolidHighContrast = Template.bind({})
+SolidHighContrast.args = {
+    id:'solidButton',
+    name:'Solid-High-Contrast',
+    label:'Solid High Contrast',
+    size:'large',
+    theme:'dark',
+    hasIcon: true,
+    disabled:false,
+}
+export const SolidLowContrast = Template.bind({})
+SolidLowContrast.args = {
+    id:'solidButton',
+    name:'Solid-Low-Contrast',
+    label:'Solid Low Contrast',
+    size:'large',
+    theme:'light',
+    hasIcon: true,
+    disabled:false,
+}
+
+export const Outline = Template.bind({})
+Outline.args = {
+    id:'solidButton',
+    name:'Outline',
+    label:'Outline',
+    size:'large',
+    theme:'light',
+    hasIcon: true,
+    disabled:false,
+}
+
+export const Ghost = Template.bind({})
+Ghost.args = {
+    id:'solidButton',
+    name:'Ghost',
+    label:'Ghost',
+    size:'large',
+    theme:'light',
+    hasIcon: true,
+    disabled:false,
+}
---
+++ src/stories/Button.stories.js
@@ -0,0 +1,57 @@
+// import Button from "../components/Button";
+import { Button } from '@hmoussa1995/react-button-lib-github';
+// import { AwesomeButton } from "react-awesome-button";
+export default {
+    title: 'Button',
+    component: Button,
+    argTypes: {
+        handleClick: { action: 'handleClick' },
+        size: { control: { type: "select", options: ["large", "default", "small", "xsmall"] }},
+        theme: {control: { type: "select", options: ["light", "dark"]}}
+    }
+}
+
+const Template = args => <Button {...args} />;
+
+export const SolidHighContrast = Template.bind({})
+SolidHighContrast.args = {
+    id:'solidButton',
+    variant:'Solid-High-Contrast',
+    label:'Solid High Contrast',
+    size:'large',
+    theme:'dark',
+    hasIcon: true,
+    disabled:false,
+}
+export const SolidLowContrast = Template.bind({})
+SolidLowContrast.args = {
+    id:'solidButton',
+    variant:'Solid-Low-Contrast',
+    label:'Solid Low Contrast',
+    size:'large',
+    theme:'light',
+    hasIcon: true,
+    disabled:false,
+}
+
+export const Outline = Template.bind({})
+Outline.args = {
+    id:'solidButton',
+    variant:'Outline',
+    label:'Outline',
+    size:'large',
+    theme:'light',
+    hasIcon: true,
+    disabled:false,
+}
+
+export const Ghost = Template.bind({})
+Ghost.args = {
+    id:'solidButton',
+    variant:'Ghost',
+    label:'Ghost',
+    size:'large',
+    theme:'light',
+    hasIcon: true,
+    disabled:false,
+}
