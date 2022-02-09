d937ff49ea3f7e8cb4cd6723a37761c0e0c9fa15
Henry Clark <Henry.Clark@intusurg.com>
2022-02-09 09:08:52 -0800

    added all buttons and checkbox group

--- src/stories/Checkbox.stories.js
+++ src/stories/Checkbox.stories.js
@@ -1,21 +1,22 @@
-import { IsiCheckbox } from '@hmoussa1995/react-button-lib-github';
+import { IsiCheckbox, IsiCheckboxGroup } from '@hmoussa1995/react-button-lib-github';
 
 export default {
     title: 'IsiCheckbox',
-    component: IsiCheckbox,
+    component: IsiCheckboxGroup,
     argTypes: {
         theme: {control: { type: "select", options: ["light", "dark"]}},
-        handleChange: { action: 'handleChange' },
     }
 }
 
 
-const Template = args => <IsiCheckbox {...args} />;
+const Template = args => <IsiCheckboxGroup {...args} >
+    <IsiCheckbox label="I Confirm"/>
+    <IsiCheckbox label="I Agree"/>
+    <IsiCheckbox label="I have read the terms and conditions"/>
+</IsiCheckboxGroup>;
 
 export const Checkbox = Template.bind({})
 Checkbox.args = {
-    label:'I confirm',
-    disabled:false,
     checked:false,
     theme:'light'
 }
