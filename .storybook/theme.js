22df0c0982b682e2447a9d5b5b80b19080048b21
Vinesh kumar <Vinesh.Kumar@intusurg.com>
2022-02-09 13:04:31 -0800

    Updated to show intuitive logo

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+import logo from '../logo.svg'
+
+export default create({
+    base: 'light',
+    brandTitle: 'My custom storybook',
+    brandUrl: 'https://example.com',
+    brandImage: 'https://blue-images-isi.imgix.net/en-us/-/media/ISI/Intuitive/Card/Press-Resources/intuitive-logo-black-1060874-low-res-.png?q=100&w=540&h=360&auto=compress%2Cformat&fit=crop&crop=entropy&s=aba81d33a7bfae61c4bca9009d19890a'
+});
